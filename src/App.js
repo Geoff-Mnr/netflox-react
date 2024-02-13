1eb0c90d641305572e732df4146519d877b38e2a
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-02-13 19:55:56 +0100

    add List component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import List from './components/List';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=ca4729b6b0abadaf808f61880cf58c37');
+    const data = await response.json();
+    setMovies(data.results);
+  }
+
+  console.log(movies);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {movies && movies.map((movie) => {
+            return <List movies={movies} />
+          }
+          )}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Nav = () => {
+
+    return (
+        <nav>
+        <h1>Netflox</h1>
+        </nav>
+    );
+    }
