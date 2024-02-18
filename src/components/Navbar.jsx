7175717a8c80ab6f00586150fabf79b8beb12c3d
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-02-18 11:00:43 +0100

    add component logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import Logo from "../components/logo";
 
 const Navbar = () => {
-    
+
 
     return (
         <header className="w-full">
             <nav className="w-full bg-zinc-800 p-4 flex items-center justify-between h-1/12">
                 <div className="menu w-1/4 flex justify-between items-center">
+                    <Logo />
                     <ul className="flex text-white text-base">
                         <li><NavLink to="/" className="p-3 hover:font-semibold">Accueil</NavLink></li>
                         <li><NavLink to="/movies" className="p-3 hover:font-semibold">Films</NavLink></li>
                     </ul>
-                </div>   
+                </div>
             </nav>
+
         </header>
     );
 };
